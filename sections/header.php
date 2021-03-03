542b0db4c4b13dbd91652c987857b753fe928cdc
beltenebror <lasupercintia@gmail.com>
2021-03-03 15:30:18 +0100

    pone e en vez de en

--- sections/header.php
+++ sections/header.php
@@ -51,7 +51,7 @@
                   <button onclick="window.location.href='<?=$_SERVER['PHP_SELF']?>?lang=es'" class="es-button" style="height: 30px;width: 30px;border-radius:50%;border:none;background-image:url('../images/lenguages/espana.svg');background-repeat: no-repeat;"  > &nbsp; </button>
                 </li>
                 <li class="nav-item">
-                <button onclick="window.location.href='<?=$_SERVER['PHP_SELF']?>?lang=e'" class="en-button" style="height: 30px; width: 30px; border-radius:50%; border:none; background-image: url('images/lenguages/reino-unido.svg');   background-repeat: no-repeat"> &nbsp; </button>
+                <button onclick="window.location.href='<?=$_SERVER['PHP_SELF']?>?lang=en'" class="en-button" style="height: 30px; width: 30px; border-radius:50%; border:none; background-image: url('images/lenguages/reino-unido.svg');   background-repeat: no-repeat"> &nbsp; </button>
                 </li>
               </ul>
             </div>
