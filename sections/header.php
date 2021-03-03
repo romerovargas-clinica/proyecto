39c3730a62117c6bbf63517b6201403e8a5a1740
beltenebror <lasupercintia@gmail.com>
2021-03-03 15:11:42 +0100

    botones de idioma, aunque el css está directo, no consigo ponerlo con una clase

--- sections/header.php
+++ sections/header.php
@@ -47,6 +47,12 @@
                     </a>
                   <?php endif; ?>
                 </li>
+                <li class="nav-item">
+                  <button onclick="window.location.href='<?=$_SERVER['PHP_SELF']?>?lang=es'" class="es-button" style="height: 30px;width: 30px;border-radius:50%;border:none;background-image:url('../images/lenguages/espana.svg');background-repeat: no-repeat;"  > &nbsp; </button>
+                </li>
+                <li class="nav-item">
+                <button onclick="window.location.href='<?=$_SERVER['PHP_SELF']?>?lang=en'" class="en-button" style="height: 30px; width: 30px; border-radius:50%; border:none; background-image: url('images/lenguages/reino-unido.svg');   background-repeat: no-repeat"> &nbsp; </button>
+                </li>
               </ul>
             </div>
           </div>
@@ -65,59 +71,59 @@
         <div class="row">
           <div class="col-lg-12">
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-              
-                <a class="navbar-brand" href="index.php">
-                  <img src="assets/img/logo/logo-dark.png" style="width:280px" alt="Logo">
-                </a>
 
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="toggler-icon"></span>
-                  <span class="toggler-icon"></span>
-                  <span class="toggler-icon"></span>                    
-                </button>
+              <a class="navbar-brand" href="index.php">
+                <img src="assets/img/logo/logo-dark.png" style="width:280px" alt="Logo">
+              </a>
 
-                <div class="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">
-                  <ul id="nav" class="navbar-nav ml-auto">
-                  
-                    <li class="nav-item">
-                      <a class="nav-link" href="<?= $href ?>#about"><?= __('mn_AboutUs', $lang) ?></a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="<?= $href ?>#services"><?= __('mn_Speciality', $lang) ?></a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="<?= $href ?>#team"><?= __('mn_Team', $lang) ?></a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="<?= $href ?>#blog"><?= __('mn_News', $lang) ?></a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="<?= $href ?>#contact"><?= __('mn_Contact', $lang) ?></a>
-                    </li>
-                    <?php if (isAuthenticated()) : ?>
-                      <!-- Menu Usuario -->
-                      <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                          <?= $_SESSION['firstname'] ?>
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                          <li><a class="dropdown-item" href="cites.php"><?= __('mn_Cites', $lang) ?></a></li>
-                          <?php if (isset($_SESSION["roles"]) && $_SESSION["roles"] == "[ADMIN-USER]") : ?>
-                            <li>
-                              <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="admin.php"><?= __('mn_Admin', $lang) ?></a></li>
-                            <li><a class="dropdown-item" href="budget.php"><?= __('mn_budget', $lang) ?></a></li>
-                          <?php endif; ?>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="toggler-icon"></span>
+                <span class="toggler-icon"></span>
+                <span class="toggler-icon"></span>
+              </button>
+
+              <div class="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">
+                <ul id="nav" class="navbar-nav ml-auto">
+
+                  <li class="nav-item">
+                    <a class="nav-link" href="<?= $href ?>#about"><?= __('mn_AboutUs', $lang) ?></a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="<?= $href ?>#services"><?= __('mn_Speciality', $lang) ?></a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="<?= $href ?>#team"><?= __('mn_Team', $lang) ?></a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="<?= $href ?>#blog"><?= __('mn_News', $lang) ?></a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="<?= $href ?>#contact"><?= __('mn_Contact', $lang) ?></a>
+                  </li>
+                  <?php if (isAuthenticated()) : ?>
+                    <!-- Menu Usuario -->
+                    <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <?= $_SESSION['firstname'] ?>
+                      </a>
+                      <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="cites.php"><?= __('mn_Cites', $lang) ?></a></li>
+                        <?php if (isset($_SESSION["roles"]) && $_SESSION["roles"] == "[ADMIN-USER]") : ?>
                           <li>
                             <hr class="dropdown-divider">
                           </li>
-                          <li><a class="dropdown-item" href="logout.php"><?= __('mn_Logout', $lang) ?></a></li>
-                        </ul>
-                      </li>
-                    <?php endif; ?>
-                </div>
-              
+                          <li><a class="dropdown-item" href="admin.php"><?= __('mn_Admin', $lang) ?></a></li>
+                          <li><a class="dropdown-item" href="budget.php"><?= __('mn_budget', $lang) ?></a></li>
+                        <?php endif; ?>
+                        <li>
+                          <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="logout.php"><?= __('mn_Logout', $lang) ?></a></li>
+                      </ul>
+                    </li>
+                  <?php endif; ?>
+              </div>
+
             </nav>
           </div>
         </div>
@@ -126,3 +132,4 @@
   </div>
 </header>
 <!-- ========================= header end ========================= -->
+
