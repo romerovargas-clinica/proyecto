b0a46e7c6fe3bfbad1c367d9f6bf94198863292f
david <davidbermudez@jerez.es>
2021-03-03 08:41:38 +0100

    Chat

--- includes/scripts.php
+++ includes/scripts.php
@@ -29,3 +29,19 @@
 				});
 				*/
 	</script>
+
+	<script>
+		(function(d, t) {
+			var BASE_URL = "https://app.chatwoot.com";
+			var g = d.createElement(t),
+				s = d.getElementsByTagName(t)[0];
+			g.src = BASE_URL + "/packs/js/sdk.js";
+			s.parentNode.insertBefore(g, s);
+			g.onload = function() {
+				window.chatwootSDK.run({
+					websiteToken: 'gztoHeTAfwnfs3QgWXXovkEy',
+					baseUrl: BASE_URL
+				})
+			}
+		})(document, "script");
+	</script>
